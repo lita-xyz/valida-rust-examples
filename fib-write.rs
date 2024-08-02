0f26f1d42f6f557b5d230a653873280e8ea00f5b
thealmarty <thealmartyblog@gmail.com>
2024-08-02 09:58:30 -0700

    Update fib-write.rs to write more than 1 byte.

--- fib-write.rs
+++ fib-write.rs
@@ -11,9 +11,15 @@ fn panic(_info: &PanicInfo) -> ! {
 #[start]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
 
-    extern { fn read_stdin() -> u32;}
-    extern { fn write_stdout(n: u32);}
-
+    extern { fn read_stdin() -> u8;}
+    extern { fn write_stdout(n: u8);}
+    fn transform_u32_to_array_of_u8(x:u32) -> [u8;4] {
+        let b1 : u8 = ((x >> 24) & 0xff) as u8;
+        let b2 : u8 = ((x >> 16) & 0xff) as u8;
+        let b3 : u8 = ((x >> 8) & 0xff) as u8;
+        let b4 : u8 = (x & 0xff) as u8;
+        return [b4, b3, b2, b1]
+    }
     unsafe {
     let n = read_stdin();
     let mut a: u32 = 0;
@@ -24,8 +30,10 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize {
         a = b;
         b = sum;
         }
-    write_stdout(b);
+    let b_as_bytes: [u8; 4] = transform_u32_to_array_of_u8(b);
+    for i in 0..4 {
+        write_stdout(b_as_bytes[i]);
+        }
     }
     return 0;
 }
-
